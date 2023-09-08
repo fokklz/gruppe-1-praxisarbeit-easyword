740cd9f2788b566c0dde5943e3e7b0f35c10d4cb
Bobby <138381572+BiluliB@users.noreply.github.com>
2023-09-08 12:13:41 +0200

    add Feedback

--- EasyWord/MainWindow.xaml.cs
+++ EasyWord/MainWindow.xaml.cs
@@ -66,12 +66,74 @@ namespace EasyWord
             }
         }
 
-        private void infoDev_click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Word verification when button pressed and show button next and hide button Check
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCheck_Click(object sender, RoutedEventArgs e)
         {
-            devVersion customDialog = new devVersion();
+            CheckWordMatch();
+            enWordInput.IsReadOnly = true;
+            btnNext.Visibility = Visibility.Visible;
+            btnCheck.Visibility = Visibility.Hidden;
+            btnNext.Focus();
+        }
 
-            // Show the dialog as a modal dialog
-            customDialog.ShowDialog();
+
+        /// <summary>
+        /// Word verficateion when Enter pressed and show button next and hide butotn Check
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void enWordInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                CheckWordMatch();
+                enWordInput.IsReadOnly = true;
+                btnNext.Visibility = Visibility.Visible;
+                btnCheck.Visibility = Visibility.Hidden;
+                btnNext.Focus();
+            }
+        }
+
+        /// <summary>
+        /// Clear input, hide button Next and show button Check
+        /// change border and background color back to standard
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            enWordInput.Text = "";
+            enWordInput.IsReadOnly = false;
+            btnNext.Visibility = Visibility.Hidden;
+            btnCheck.Visibility = Visibility.Visible;
+            enWordInput.Focus();
+            enWordInput.BorderBrush = Brushes.Black;
+            enWordInput.Background = Brushes.White;
+
+        }
+
+        /// <summary>
+        /// Check word match from label and input and 
+        /// change color to the corresponding input
+        /// right = green false = red
+        /// </summary>
+
+        private void CheckWordMatch()
+        {
+            if (enWordInput.Text == deWordOutput.Content.ToString())
+            {
+                enWordInput.BorderBrush = Brushes.Green;
+                enWordInput.Background = new SolidColorBrush(Color.FromArgb(50, 0, 255, 0));
+            }
+            else
+            {
+                enWordInput.BorderBrush = Brushes.Red;
+                enWordInput.Background = new SolidColorBrush(Color.FromArgb(50, 255, 0, 0));
+            }
         }
     }
 }
