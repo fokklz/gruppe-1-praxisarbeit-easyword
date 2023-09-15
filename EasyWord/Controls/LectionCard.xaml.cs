63d4da7281a6c12f0eef8c04a7d1a2e172f3c604
mahgoe <gonenmahir@hotmail.com>
2023-09-15 18:42:28 +0200

    init Lectioncard

---
+++ EasyWord/Controls/LectionCard.xaml.cs
@@ -0,0 +1,36 @@
+﻿using EasyWord.Common;
+using EasyWord.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EasyWord.Controls
+{
+    /// <summary>
+    /// Interaction logic for LectionCard.xaml
+    /// </summary>
+    public partial class LectionCard : UserControl
+    {
+        public string Lection { get; set; } = "Standard";
+        public int WordCount { get; set; } = 0;
+
+        public LectionCard()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+
+    }
+}
