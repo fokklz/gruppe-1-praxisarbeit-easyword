d99df6d574d29fb51e472068820408a63f95478f
ArdaBs <134811694+ArdaBs@users.noreply.github.com>
2023-09-18 21:38:04 +0200

    implemented editing

---
+++ EasyWord/Controls/CreateOrModify.xaml.cs
@@ -0,0 +1,153 @@
+﻿using EasyWord.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EasyWord.Controls
+{
+    /// <summary>
+    /// Interaktionslogik für CreateOrModify.xaml
+    /// </summary>
+    public partial class CreateOrModify : UserControl, INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Switch to creation
+        /// </summary>
+        public bool CreateMode
+        {
+            get => _createMode;
+            set
+            {
+                _createMode = value;
+                Word = new Word();
+                OnPropertyChanged();
+            }
+        }
+        private bool _createMode = false;
+
+        /// <summary>
+        /// Current word
+        /// </summary>
+        public Word Word
+        {
+            get => _word;
+            set
+            {
+                _originalWord = new Word(value.German, value.Translation, value.Language, value.Lecture);
+                _word = value;
+                OnPropertyChanged();
+                OnPropertyChanged("QuestionLabel");
+                OnPropertyChanged("AnswerLabel");
+            }
+        }
+        private Word _word = new Word();
+        private Word _originalWord = new Word();
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// Get label for the question
+        /// based on current translation direction
+        /// </summary>
+        public string QuestionLabel
+        {
+            get
+            {
+                string translationLabel = CreateMode ? App.Language : Word.Language;
+                return App.Config.TranslationDirection ? translationLabel : "Deutsch";
+            }
+        }
+
+        /// <summary>
+        /// Get label for the answer
+        /// based on current translation direction
+        /// </summary>
+        public string AnswerLabel
+        {
+            get
+            {
+                string translationLabel = CreateMode ? App.Language : Word.Language;
+                return App.Config.TranslationDirection ? "Deutsch" : translationLabel;
+            }
+        }
+
+        public CreateOrModify()
+        {
+            DataContext = this;
+            InitializeComponent();
+            App.SessionUpdated += App_SessionUpdated;
+        }
+
+        /// <summary>
+        /// Helper to invoke property change event
+        /// </summary>
+        /// <param name="propertyName"></param>
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Handler for app session changes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void App_SessionUpdated(object? sender, EventArgs e)
+        {
+            if (App.Session == null) return;
+            App.Session.Next += (sender, e) =>
+            {
+                Word = e.CurrentWord;
+            };
+            Word = App.Session.GetNextWord() ?? new Word();
+        }
+
+        /// <summary>
+        /// Handler for language selection change
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SelectLanguage_LanguageChanged(object sender, LanguageChangedEventArgs e)
+        {
+            Word.Language = e.SelectedLanguage;
+            OnPropertyChanged("QuestionLabel");
+            OnPropertyChanged("AnswerLabel");
+            OnPropertyChanged("Word");
+        }
+
+        /// <summary>
+        /// Handler to reset the changes and switch session mode on cancel
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Word.Question = _originalWord.Question;
+            Word.Answer = _originalWord.Answer;
+            Word.Lecture = _originalWord.Lecture;
+            Word.Language = _originalWord.Language;
+            OnPropertyChanged("QuestionLabel");
+            OnPropertyChanged("AnswerLabel");
+            OnPropertyChanged("Word");
+            App.Config.SessionMode = 0;
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            App.Config.SessionMode = 0;
+        }
+    }
+}
---
+++ EasyWord/Controls/LearninCard.xaml.cs
@@ -0,0 +1,80 @@
+﻿using EasyWord.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EasyWord.Controls
+{
+    /// <summary>
+    /// Interaktionslogik für LearninCard.xaml
+    /// </summary>
+    public partial class LearninCard : UserControl, INotifyPropertyChanged
+    {
+        public Word Word
+        {
+            get => _word;
+            set
+            {
+                _word = value;
+                OnPropertyChanged();
+            }
+        }
+        private Word _word;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public LearninCard()
+        {
+            InitializeComponent();
+            DataContext = this;
+            App.SessionUpdated += App_SessionChanged;
+        }
+
+        /// <summary>
+        /// Helper to invoke property changes
+        /// </summary>
+        /// <param name="propertyName"></param>
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Handles session changes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void App_SessionChanged(object? sender, EventArgs e)
+        {
+            if (App.Session == null) return;
+            App.Session.Next += (sender, e) =>
+            {
+                Word = e.CurrentWord;
+            };
+            Word = App.Session.GetNextWord() ?? new Word();
+        }
+
+        /// <summary>
+        /// Handler to set Sessionmode to editing
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            App.Config.SessionMode = 1;
+        }
+    }
+}
